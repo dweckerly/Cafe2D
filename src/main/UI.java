00dc3f190db350d1c948f05437f844dfcb6a5da1
dweckerly <d.weckerly@gmail.com>
2024-05-17 08:35:18 -0500

    new font

--- src/main/UI.java
+++ src/main/UI.java
@@ -1,14 +1,13 @@
 package main;
 
-import object.Object_Key;
-
 import java.awt.*;
-import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class UI {
     GamePanel gp;
     Graphics2D g2;
-    Font arial40;
+    Font baseFont;
     public boolean messageOn = false;
     public String message = "";
     int messageCounter = 0;
@@ -18,9 +17,17 @@ public class UI {
 
     public String currentDialogue = "";
 
-    public UI(GamePanel gp) {
+    public UI(GamePanel gp){
         this.gp = gp;
-        arial40 = new Font("Arial", Font.PLAIN, 40);
+
+        try {
+            InputStream is = getClass().getResourceAsStream("/fonts/base.ttf");
+            baseFont = Font.createFont(Font.TRUETYPE_FONT, is);
+        } catch (FontFormatException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void showMessage(String message) {
@@ -30,7 +37,7 @@ public class UI {
 
     public void draw(Graphics2D g2) {
         this.g2 = g2;
-        g2.setFont(arial40);
+        g2.setFont(baseFont);
         g2.setColor(Color.white);
 
         if (gp.gameState == gp.playState) {
@@ -59,12 +66,12 @@ public class UI {
         int width = gp.screenWidth - (gp.tileSize * 4);
         int height = gp.tileSize * 4;
         drawUIWindow(x, y, width, height);
-        g2.setFont(g2.getFont().deriveFont(Font.PLAIN, 32));
+        g2.setFont(g2.getFont().deriveFont(Font.PLAIN, gp.tileSize / 2));
         x += gp.tileSize;
         y += gp.tileSize;
         for (String line : currentDialogue.split("\n")) {
             g2.drawString(line, x, y);
-            y += gp.tileSize;
+            y += gp.tileSize / 2;
         }
     }
 
